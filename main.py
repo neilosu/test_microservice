bfd69544c8b01684efd867c8e648797b085bb606
Neil Chou <neil@valtec.io>
2024-02-27 00:38:06 +0800

    dev

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ url = "http://127.0.0.1:5000/db/acquire_unit"
 
 payload = json.dumps({
   "list": 1,
-  "unit": 1,
+  "unit": 6,
   "attribute": [
     "word",
     "sentence"
   ]
 })
 headers = {
-  'Content-Type': 'application/json'
+    'Content-Type': 'application/json'
 }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+response = requests.request("POST", url, headers=headers, data=payload, timeout=10)
 
 result = response.json()
 result_dict = json.loads(result)
